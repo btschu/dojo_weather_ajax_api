b40add84fe3b25917caf194e2e5b3aaaf50166fd
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2022-01-26 00:11:27 -0700

    need to fix conversion

--- script.js
+++ script.js
@@ -9,13 +9,24 @@ function acceptCookies() {
 
 async function locationChange() {
     var city = document.querySelector('.city').value;
-    var response = await fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=fff5ccc58d0a63c089e3b0ff100a7559");
+    var response = await fetch( "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=fff5ccc58d0a63c089e3b0ff100a7559");
     var data = await response.json();
     console.log(data);
-    console.log(city);
+    // adds city name at the top
     document.getElementsByTagName('h2')[0].innerText = city;
-    document.getElementsByTagName('h3')[0].innerText = list.dt;
-    // document.getElementsByTagName('img')[0].setAttribute("src", data.avatar_url);
+    // adds today's date at the top of the weather card
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    document.getElementsByTagName('h3')[0].innerText = (new Intl.DateTimeFormat('en-US', options).format());
+    // adds an image for the current weather
+    var icon = data.weather[0].icon
+    document.querySelector('#weatherImg').setAttribute ("src", "http://openweathermap.org/img/wn/" + icon + "@2x.png");
+    // adds a description of the current weather
+    document.getElementsByTagName('h4')[0].innerText = data.weather[0].description;
+    // displays current temp
+    document.querySelector('#currentTemp').innerText = "Current Temperature\n" + data.main.temp_max + "°";
+    // displays the high and low temp for the day
+    document.querySelector('#high').innerText = data.main.temp_max + "°";
+    document.querySelector('#low').innerText = data.main.temp_min + "°";
     return data;
 }
 
@@ -51,4 +62,3 @@ input.addEventListener("keyup", function(event) {
     document.getElementById("myBtn").click();
     }
 });
-
