3665bcb17976bf52ff8939b9bcf36b354827bdf9
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2022-01-26 20:21:38 -0700

    still need to fix c to f conversion

--- script.js
+++ script.js
@@ -9,48 +9,31 @@ function acceptCookies() {
 
 async function locationChange() {
     var city = document.querySelector('.city').value;
-    var response = await fetch( "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=fff5ccc58d0a63c089e3b0ff100a7559");
+    var response = await fetch( "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&units=metric&appid=fff5ccc58d0a63c089e3b0ff100a7559");
     var data = await response.json();
     console.log(data);
+
     // adds city name at the top
     document.getElementsByTagName('h2')[0].innerText = city;
+
     // adds today's date at the top of the weather card
     var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     document.getElementsByTagName('h3')[0].innerText = (new Intl.DateTimeFormat('en-US', options).format());
+
     // adds an image for the current weather
     var icon = data.weather[0].icon
     document.querySelector('#weatherImg').setAttribute ("src", "http://openweathermap.org/img/wn/" + icon + "@2x.png");
+
     // adds a description of the current weather
     document.getElementsByTagName('h4')[0].innerText = data.weather[0].description;
+
     // displays current temp
-    document.querySelector('#currentTemp').innerText = "Current Temperature\n" + data.main.temp_max + "°";
+    document.querySelector('#currentTemp').innerText = "Current Temp\n" + data.main.temp_max + "°";
+
     // displays the high and low temp for the day
-    document.querySelector('#high').innerText = data.main.temp_max + "°";
-    document.querySelector('#low').innerText = data.main.temp_min + "°";
+    document.querySelector('#high').innerText = "High:" + data.main.temp_max + "°";
+    document.querySelector('#low').innerText = "Low:" + data.main.temp_min + "°";
     return data;
-}
-
-// change between celsius and fahrenheit
-
-function celsiusToFahrenheit(temp) {
-    return Math.round(9 / 5 * temp + 32)
-}
-
-function fahrenheitToCelsius(temp) {
-    return Math.round(5 / 9 * (temp - 32))
-}
-
-function changeTemp(element) {
-    console.log(element.value)
-    for(let i=1; i<9; i++) {
-        let tempSpan = document.querySelector("#temp" + i);
-        let tempVal = parseInt(tempSpan.innerText)
-        if(element.value == "°C") {
-            tempSpan.innerText = fahrenheitToCelsius(tempVal)
-        } else {
-            tempSpan.innerText = celsiusToFahrenheit(tempVal)
-        }
-    }
 }
 
 // trigger button when pressing enter key
